67de905a774672c03fbdc47c8336839bea0564c9
Ewemor Martin <martinewemor@gmail.com>
2023-10-19 14:37:54 +0200

    Tesztverzio parameterezese

--- src/pizzagui_hf/PizzaGui.java
+++ src/pizzagui_hf/PizzaGui.java
@@ -338,7 +338,7 @@ public class PizzaGui extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        JOptionPane.showMessageDialog(rootPane, "Tesztverzió");
+        JOptionPane.showMessageDialog(rootPane, "Tesztverzió", "Cím:", 2);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
